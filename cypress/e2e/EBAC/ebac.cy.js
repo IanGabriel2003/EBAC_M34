3aaae6b01776e405af0ca0fd63c4f7e894bfb0e7
Ian Gabriel Pereira de Queiroz <iangabrielpq392@gmail.com>
2024-09-04 19:24:06 -0300

    Melhora no código

--- cypress/e2e/EBAC/ebac.cy.js
+++ cypress/e2e/EBAC/ebac.cy.js
@@ -1,25 +1,25 @@
-<reference types="cypress" />
+/// <reference types="cypress" />
 
 describe('Teste para o site', () => {
     beforeEach(() => {
         cy.visit('https://agenda-contatos-react.vercel.app/')
     })
 
-    it('Deve editar o primeiro 1 ALTERAÇÃO', () => {
+    it('Deve editar o primeiro contato', () => {
         cy.get('.sc-beqWaB.eQdhbg.contato').first().click()
         cy.get('.edit').click()
-        cy.get('input[type="text"]').type('Nome Teste')
-        cy.get('input[type="email"]').type('emailteste@gmail.com')
-        cy.get('input[type="tel"]').type('01 23456789')
+        cy.get('input[type="text"]').clear().type('Nome Teste')
+        cy.get('input[type="email"]').clear().type('emailteste@gmail.com')
+        cy.get('input[type="tel"]').clear().type('01 23456789')
         cy.get('.alterar').click()
     })
 
-    it('Deve excluir o 2 REMOÇÃO DE UM CONTATO', () => {
-        cy.get('.sc-beqWaB.eQdhbg.contato').second().click()
+    it('Deve excluir o segundo contato', () => {
+        cy.get('.sc-beqWaB.eQdhbg.contato').eq(1).click()
         cy.get('.delete').click()
     })
 
-    it('Deve adicionar 1 INLCUSÃO', () => {
+    it('Deve adicionar um novo contato', () => {
         cy.get('input[type="text"]').type('Nome Teste2')
         cy.get('input[type="email"]').type('emailteste2@gmail.com')
         cy.get('input[type="tel"]').type('12 34567890')
