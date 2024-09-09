28a378816a3da390ad8e6886f3ccf959fd98a67e
Ian Gabriel Pereira de Queiroz <iangabrielpq392@gmail.com>
2024-09-09 14:09:20 -0300

    Teste Concertado

--- cypress/e2e/EBAC/ebac.cy.js
+++ cypress/e2e/EBAC/ebac.cy.js
@@ -5,24 +5,28 @@ describe('Teste para o site', () => {
         cy.visit('https://agenda-contatos-react.vercel.app/')
     })
 
-    it('Deve editar o primeiro contato', () => {
-        cy.get('.sc-beqWaB.eQdhbg.contato').first().click()
-        cy.get('.edit').click()
-        cy.get('input[type="text"]').clear().type('Nome Teste')
-        cy.get('input[type="email"]').clear().type('emailteste@gmail.com')
-        cy.get('input[type="tel"]').clear().type('01 23456789')
-        cy.get('.alterar').click()
+    it('Deve excluir 2 contatos', () => {
+        cy.get('.delete').eq(1).click()
+        cy.get('.delete').eq(0).click()
     })
 
-    it('Deve excluir o segundo contato', () => {
-        cy.get('.sc-beqWaB.eQdhbg.contato').eq(1).click()
-        cy.get('.delete').click()
-    })
-
-    it('Deve adicionar um novo contato', () => {
-        cy.get('input[type="text"]').type('Nome Teste2')
-        cy.get('input[type="email"]').type('emailteste2@gmail.com')
+    it('Deve adicionar 2 novos contato', () => {
+        cy.get('input[type="text"]').type('Ian Gabriel')
+        cy.get('input[type="email"]').type('emailteste@gmail.com')
         cy.get('input[type="tel"]').type('12 34567890')
         cy.get('.adicionar').click()
+
+        cy.get('input[type="text"]').type('Gabriel Ian')
+        cy.get('input[type="email"]').type('emailteste2@gmail.com')
+        cy.get('input[type="tel"]').type('01 23456789')
+        cy.get('.adicionar').click()
+    })
+
+    it('Deve editar o primeiro contato', () => {
+        cy.get('.edit').first().click()
+        cy.get('input[type="text"]').clear().type('Pedro')
+        cy.get('input[type="email"]').clear().type('pedrin@gmail.com')
+        cy.get('input[type="tel"]').clear().type('23 45678901')
+        cy.get('.alterar').click()
     })
 })
